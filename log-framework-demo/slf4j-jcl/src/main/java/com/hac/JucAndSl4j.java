b3874b415c0affd35335c0e5634b564139f59389
HaC <huangyongwen0306@163.com>
2023-11-30 18:37:13 +0800

    add demo

--- log-framework-demo/slf4j-jcl/src/main/java/com/hac/JucAndSl4j.java
+++ log-framework-demo/slf4j-jcl/src/main/java/com/hac/JucAndSl4j.java
@@ -6,13 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JucAndSl4j {
-//    java common logging
+    //    java common logging
     private static final Log JUC = LogFactory.getLog(JucAndSl4j.class);
     // sl4j
     private static final Logger Sl4j = LoggerFactory.getLogger(JucAndSl4j.class);
 
     public static void main(String[] args) {
         JUC.info("JUC");
-        Sl4j.info("Sl4j");
+        Sl4j.info("{}", "Sl4j");
     }
 }
